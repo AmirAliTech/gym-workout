0a127fd1d6713138359c2914a75fe4d6ba9b0926
lazybluff <131444811+lazybluff@users.noreply.github.com>
2023-12-05 06:21:46 -0800

    Add files via upload

---
+++ website/bootstrap website 2/script.js
@@ -0,0 +1,7 @@
+// const third = document.getElementById(third);
+// const fourth = document.getElementById(fourth);
+// const fifth = document.getElementById(fifth);
+
+// function one(){
+//     document.getElementById(move1).classList.add("myphoto")
+// }
